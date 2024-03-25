21970a0eb8356280e4372588a6add57a7ddd6344
Kai Volland <volland@terrestris.de>
2024-03-25 17:42:16 +0100

    feat: add support for ShapefileDataStore

--- src/main/kotlin/de/terrestris/geoserver/wps/TimeDimension.kt
+++ src/main/kotlin/de/terrestris/geoserver/wps/TimeDimension.kt
@@ -25,6 +25,8 @@ import org.geoserver.security.decorators.ReadOnlyDataStore
 import org.geoserver.wps.gs.GeoServerProcess
 import org.geoserver.wps.process.RawData
 import org.geoserver.wps.process.StringRawData
+import org.geotools.data.shapefile.ShapefileDataStore
+import org.geotools.data.store.ContentDataStore
 import org.geotools.jdbc.JDBCDataStore
 import org.geotools.process.factory.DescribeParameter
 import org.geotools.process.factory.DescribeProcess
@@ -34,7 +36,10 @@ import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 import java.sql.SQLException
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.logging.Level
+
 
 @DescribeProcess(title = "timeDimension", description = "Gets the time dimension of a layer.")
 class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
@@ -60,6 +65,7 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
         var stmt: PreparedStatement? = null
         var rs: ResultSet? = null
         return try {
+            LOGGER.info("Getting time dimensions for layer: $layerName")
             val factory = JsonNodeFactory(false)
             val root = factory.objectNode()
             val tableName = layerName.split(":".toRegex())[1]
@@ -67,47 +73,87 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
                 geoServer.catalog.getFeatureTypeByName(layerName) ?: return error("Feature type not found.")
             val source = featureType.getFeatureSource(null, null) ?: return error("Source not found.")
             var store = source.dataStore
-            if (store !is JDBCDataStore && store !is ReadOnlyDataStore) {
-                return error("Store is not a JDBC data store: ${store.javaClass.canonicalName}")
-            }
-            if (store is ReadOnlyDataStore) {
-                store = store.unwrap(JDBCDataStore::class.java)
-            }
-            if (store !is JDBCDataStore) {
-                return error("Potentially wrapped store is not a JDBC data store: ${store.javaClass.canonicalName}")
-            }
-            val dataStore = store
-            val schema = dataStore.databaseSchema
-            conn = dataStore.dataSource.connection
-            var sql: String
 
+            if (store is ReadOnlyDataStore) {
+                store = unwrapDataStore(store)
+            }
+            if (store !is JDBCDataStore && store !is ShapefileDataStore) {
+                return error("Store of type ${store.javaClass.simpleName} is not supported.")
+            }
+
+            // Get the time dimension
             val timeData: DimensionInfoImpl = featureType.metadata.get("time") as DimensionInfoImpl
             val attribute: String = timeData.attribute
             val presentation: DimensionPresentation = timeData.presentation
-
             root.put("presentation", presentation.toString())
-            if (presentation === DimensionPresentation.LIST) {
-                sql = "SELECT DISTINCT \"$attribute\" FROM $schema.\"$tableName\""
-            } else {
-                sql = "SELECT MIN(\"$attribute\"), MAX(\"$attribute\") FROM $schema.\"$tableName\""
-            }
 
-            LOGGER.fine("Final SQL: $sql")
-            stmt = conn.prepareStatement(sql)
-            rs = stmt.executeQuery()
-
-            var dataNode = root.putArray("data")
-            while (rs.next()) {
+            // Get the time values for JDBCDataStore
+            if (store is JDBCDataStore) {
+                LOGGER.info("… from JDBCDataStore");
+                val schema = store.databaseSchema
+                conn = store.dataSource.connection
+                var sql: String
                 if (presentation === DimensionPresentation.LIST) {
-                    var value = rs.getString(1)
-                    dataNode.add(value)
+                    sql = "SELECT DISTINCT \"$attribute\" FROM $schema.\"$tableName\""
                 } else {
-                    var min = rs.getString(1)
-                    var max = rs.getString(2)
-                    dataNode.add(min)
-                    dataNode.add(max)
+                    sql = "SELECT MIN(\"$attribute\"), MAX(\"$attribute\") FROM $schema.\"$tableName\""
+                }
+
+                LOGGER.fine("Final SQL: $sql")
+                stmt = conn.prepareStatement(sql)
+                rs = stmt.executeQuery()
+
+                var dataNode = root.putArray("data")
+                try {
+                    while (rs.next()) {
+                        if (presentation === DimensionPresentation.LIST) {
+                            var value = rs.getString(1)
+                            dataNode.add(value)
+                        } else {
+                            var min = rs.getString(1)
+                            var max = rs.getString(2)
+                            dataNode.add(min)
+                            dataNode.add(max)
+                        }
+                    }
+                } finally {
+                    rs.close()
                 }
             }
+
+            // Get the time values for ShapefileDataStore
+            if (store is ShapefileDataStore) {
+                LOGGER.info("… from ShapefileDataStore");
+                val featureSource = store.featureSource
+                val featureCollection = featureSource.features
+                val iterator = featureCollection.features()
+                val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ")
+
+                try {
+                    var dataNode = root.putArray("data")
+                    val distinctValues = mutableSetOf<String>()
+                    while (iterator.hasNext()) {
+                        val feature = iterator.next()
+                        val value = feature.getAttribute(attribute)
+                        if (value != null) {
+                            distinctValues.add(sdf.format(value as Date))
+                        }
+                    }
+                    if (presentation === DimensionPresentation.LIST) {
+                        distinctValues.forEach {
+                            dataNode.add(it)
+                        }
+                    } else {
+                        val min = distinctValues.minOrNull()
+                        val max = distinctValues.maxOrNull()
+                        dataNode.add(min)
+                        dataNode.add(max)
+                    }
+                } finally {
+                    iterator.close()
+                }
+            }
+
             success(root)
         } catch (e: Exception) {
             LOGGER.fine("Error when getting time dimensions: " + e.message)
@@ -118,6 +164,27 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
             stmt?.close()
             rs?.close()
         }
+    }
+
+    @Throws(IllegalArgumentException::class)
+    private fun unwrapDataStore(dataStore: ReadOnlyDataStore): ContentDataStore {
+        val dataStoreTypes = listOf(
+            JDBCDataStore::class.java,
+            ShapefileDataStore::class.java,
+        )
+
+        for (type in dataStoreTypes) {
+            try {
+                val unwrappedDataStore = dataStore.unwrap(type)
+                if (unwrappedDataStore != null) {
+                    return unwrappedDataStore
+                }
+            } catch (e: Exception) {
+                LOGGER.log(Level.FINE, "Could not unwrap data store of type ${type.simpleName}", e)
+            }
+        }
+
+        throw IllegalArgumentException("Could not unwrap data store")
     }
 
     @Throws(JsonProcessingException::class)
