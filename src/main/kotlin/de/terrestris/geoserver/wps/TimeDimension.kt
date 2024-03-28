bcd0a70e1c7ff006dd0b5b5f8e5e771c192bbad3
Kai Volland <volland@terrestris.de>
2024-03-28 09:38:20 +0100

    refactoring and more logs

--- src/main/kotlin/de/terrestris/geoserver/wps/TimeDimension.kt
+++ src/main/kotlin/de/terrestris/geoserver/wps/TimeDimension.kt
@@ -21,14 +21,12 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import org.geoserver.catalog.*
 import org.geoserver.catalog.impl.DimensionInfoImpl
 import org.geoserver.config.GeoServer
-import org.geoserver.security.decorators.ReadOnlyDataStore
 import org.geoserver.security.decorators.SecuredFeatureTypeInfo
 import org.geoserver.wps.gs.GeoServerProcess
 import org.geoserver.wps.process.RawData
 import org.geoserver.wps.process.StringRawData
 import org.geotools.data.DataStoreFinder
 import org.geotools.data.shapefile.ShapefileDataStore
-import org.geotools.data.store.ContentDataStore
 import org.geotools.jdbc.JDBCDataStore
 import org.geotools.process.factory.DescribeParameter
 import org.geotools.process.factory.DescribeProcess
@@ -36,8 +34,6 @@ import org.geotools.process.factory.DescribeResult
 import org.geotools.util.logging.Logging
 import java.io.File
 import java.net.URL
-import java.sql.Connection
-import java.sql.PreparedStatement
 import java.sql.SQLException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -52,15 +48,14 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
 
     private val objectMapper: ObjectMapper = ObjectMapper()
 
-    @DescribeResult(name = "result", description = "A json object containing two keys: \"presentation\" -> The DimensionPresentation chosen for the layer. \"data\" -> Contains the date valus.", primary = true)
+    @DescribeResult(name = "result", description = "A json object containing two keys: \"presentation\" -> The DimensionPresentation chosen for the layer. \"data\" -> Contains the date values.", primary = true)
     @Throws(
         JsonProcessingException::class, SQLException::class
     )
     fun execute(
         @DescribeParameter(
             name = "layerName",
-            description = "The qualified name of the layer to retrieve the values from. The layer must be based on a " +
-                    "JDBC/postgres datastore and be based on a single table with a name equal to the layer name."
+            description = "The qualified name of the layer to retrieve the values from."
         ) layerName: String
     ): RawData {
         return try {
@@ -69,25 +64,26 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
             val root = factory.objectNode()
             val resourceName = layerName.split(":".toRegex())[1]
             val layer = geoServer.catalog.getLayerByName(layerName)
-            var resource = layer.getResource();
-
-            if (resource == null) {
-                return error("Resource not found.")
-            }
+            val resource = layer.getResource()
+                ?: return error("Resource not found.")
 
             // Get the time dimension
-            val timeData = resource.metadata.get("time") as DimensionInfoImpl
+            val timeData = resource.metadata["time"] as DimensionInfoImpl
             val presentation = timeData.presentation
             var attribute = timeData.attribute
             val values: MutableSet<String>
 
+            LOGGER.fine("Time dimension presentation: $presentation")
+            LOGGER.fine("Time dimension attribute: $attribute")
             if (resource is CoverageInfo) {
-                attribute = getTimeAttributeFromCoverage(resource);
-                val shapefileDataStore = getShapeFileResourceFromCoverage(resource);
+                LOGGER.fine("Resource is coverage")
+                attribute = getTimeAttributeFromCoverage(resource)
+                val shapefileDataStore = getShapeFileResourceFromCoverage(resource)
                 values = if (presentation === DimensionPresentation.LIST)
-                    getDistinctValuesFromStore(shapefileDataStore, attribute, resourceName) else
-                    getMinMaxValuesFromStore(shapefileDataStore, attribute, resourceName)
+                    getDistinctValuesFromStore(shapefileDataStore, attribute) else
+                    getMinMaxValuesFromStore(shapefileDataStore, attribute)
             } else {
+                LOGGER.fine("Resource is feature type")
                 values = if (presentation === DimensionPresentation.LIST)
                     getDistinctValuesFromResource(resource, attribute, resourceName) else
                     getMinMaxValuesFromResource(resource, attribute, resourceName)
@@ -96,12 +92,11 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
             root.put("presentation", presentation.toString())
             val dataNode = root.putArray("data")
             values.forEach { dataNode.add(it) }
-            success(root);
+            success(root)
         } catch (e: Exception) {
             LOGGER.log(Level.WARNING, "Error getting time dimension", e)
             error("Error getting time dimension: ${e.message}")
         }
-
     }
 
     private fun getTimeAttributeFromCoverage(coverageInfo: CoverageInfo): String {
@@ -112,10 +107,15 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
 
         // list files in the directory
         val directory = File(baseDirectory, nativeName)
+
+        if (!directory.isDirectory) {
+            throw Error("Coverage directory not found:  ${directory.absolutePath}")
+        }
+
         val files = directory.listFiles()
 
-        // find file the ends with properties but is not indexer.properties or timeregex.properties
-        for (file in files) {
+        LOGGER.info("Looking for properties file in coverage directory: ${directory.absolutePath}")
+        for (file in files!!) {
             if (file.name.endsWith("properties") && !file.name.equals("indexer.properties") && !file.name.equals("timeregex.properties")) {
                 val url = URL("file:${file.absolutePath}")
                 val properties = Properties()
@@ -136,11 +136,10 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
         val directory = File(baseDirectory, nativeName)
 
         if (!directory.isDirectory) {
-            throw Error("Coverage directory not found")
+            throw Error("Coverage directory not found: ${directory.absolutePath}")
         }
 
         val files = directory.listFiles()
-
         // throw error if folder contains more than one file that ends with .shp
         val shapeFiles = files?.filter { it.name.endsWith(".shp") }
         if (shapeFiles === null || shapeFiles.size > 1) {
@@ -149,135 +148,96 @@ class TimeDimension(private val geoServer: GeoServer) : GeoServerProcess {
         val storeParams = mapOf(
             "url" to shapeFiles[0].toURI().toURL()
         )
-        return DataStoreFinder.getDataStore(storeParams) as ShapefileDataStore;
+        return DataStoreFinder.getDataStore(storeParams) as ShapefileDataStore
     }
 
     private fun getDistinctValuesFromResource(resource: ResourceInfo, attribute: String, tableName: String): MutableSet<String> {
         if (resource is SecuredFeatureTypeInfo || resource is FeatureTypeInfo) {
-            val dataStore = DataStoreFinder.getDataStore(resource.store.connectionParameters);
+            val dataStore = DataStoreFinder.getDataStore(resource.store.connectionParameters)
             return when (dataStore) {
                 is JDBCDataStore -> getDistinctValuesFromStore(dataStore, attribute, tableName)
-                is ShapefileDataStore -> getDistinctValuesFromStore(dataStore, attribute, tableName)
+                is ShapefileDataStore -> getDistinctValuesFromStore(dataStore, attribute)
                 else -> throw IllegalArgumentException("Unsupported data store type")
             }
         }
-        throw Error("Unsupported resource type: " + resource::class.simpleName)
+        throw Error("Unsupported resource type: ${resource::class.simpleName}")
     }
 
     private fun getMinMaxValuesFromResource(resource: ResourceInfo, attribute: String, tableName: String): MutableSet<String> {
         if (resource is SecuredFeatureTypeInfo || resource is FeatureTypeInfo) {
-            val dataStore = DataStoreFinder.getDataStore(resource.store.connectionParameters);
+            val dataStore = DataStoreFinder.getDataStore(resource.store.connectionParameters)
             return when (dataStore) {
                 is JDBCDataStore -> getMinMaxValuesFromStore(dataStore, attribute, tableName)
-                is ShapefileDataStore -> getMinMaxValuesFromStore(dataStore, attribute, tableName)
+                is ShapefileDataStore -> getMinMaxValuesFromStore(dataStore, attribute)
                 else -> throw IllegalArgumentException("Unsupported data store type")
             }
         }
-        throw Error("Unsupported resource type: " + resource::class.simpleName)
-    }
-
-    private fun getDistinctValuesFromStore(store: ReadOnlyDataStore, attribute: String, tableName: String): MutableSet<String> {
-        val unwrappedDataStore = unwrapDataStore(store)
-        return when (unwrappedDataStore) {
-            is JDBCDataStore -> getDistinctValuesFromStore(unwrappedDataStore, attribute, tableName)
-            is ShapefileDataStore -> getDistinctValuesFromStore(unwrappedDataStore, attribute, tableName)
-            else -> throw IllegalArgumentException("Unsupported data store type: " + unwrappedDataStore::class.simpleName)
-        }
+        throw Error("Unsupported resource type: ${resource::class.simpleName}")
     }
 
     private fun getDistinctValuesFromStore(store: JDBCDataStore, attribute: String, tableName: String): MutableSet<String> {
-        LOGGER.info("Get distinct values from JDBCDataStore");
-        var conn: Connection? = null
-        var stmt: PreparedStatement? = null
+        LOGGER.info("Get distinct values from JDBCDataStore")
         val schema = store.databaseSchema
-        conn = store.dataSource.connection
-        var sql = "SELECT DISTINCT \"$attribute\" FROM $schema.\"$tableName\""
+        val conn = store.dataSource.connection
+        val sql = "SELECT DISTINCT \"$attribute\" FROM $schema.\"$tableName\""
 
         LOGGER.fine("Final SQL: $sql")
-        stmt = conn.prepareStatement(sql)
-        var rs = stmt.executeQuery()
+        val stmt = conn.prepareStatement(sql)
+        val resultSet = stmt.executeQuery()
 
-        try {
+        resultSet.use { rs ->
             val distinctValues = mutableSetOf<String>()
             while (rs.next()) {
                 distinctValues.add(rs.getString(1))
             }
             return distinctValues
-        } finally {
-            rs.close()
         }
     }
 
-    private fun getDistinctValuesFromStore(store: ShapefileDataStore, attribute: String, tableName: String): MutableSet<String> {
-        LOGGER.info("Get distinct values from ShapefileDataStore");
+    private fun getDistinctValuesFromStore(store: ShapefileDataStore, attribute: String): MutableSet<String> {
+        LOGGER.info("Get distinct values from ShapefileDataStore")
         val featureSource = store.featureSource
         val featureCollection = featureSource.features
         val iterator = featureCollection.features()
         val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ")
 
-        try {
+        iterator.use { i ->
             val distinctValues = mutableSetOf<String>()
-            while (iterator.hasNext()) {
-                val feature = iterator.next()
+            while (i.hasNext()) {
+                val feature = i.next()
                 val value = feature.getAttribute(attribute)
                 if (value != null) {
                     distinctValues.add(sdf.format(value as Date))
                 }
             }
             return distinctValues
-        } finally {
-            iterator.close()
         }
     }
 
     private fun getMinMaxValuesFromStore(store: JDBCDataStore, attribute: String, tableName: String): MutableSet<String> {
-        LOGGER.info("Get min/max values from JDBCDataStore");
-        var conn: Connection? = null
-        var stmt: PreparedStatement? = null
+        LOGGER.info("Get min/max values from JDBCDataStore")
         val schema = store.databaseSchema
-        conn = store.dataSource.connection
-        var sql = "SELECT MIN(\"$attribute\"), MAX(\"$attribute\") FROM $schema.\"$tableName\""
+        val conn = store.dataSource.connection
+        val sql = "SELECT MIN(\"$attribute\"), MAX(\"$attribute\") FROM $schema.\"$tableName\""
 
         LOGGER.fine("Final SQL: $sql")
-        stmt = conn.prepareStatement(sql)
-        var rs = stmt.executeQuery()
+        val stmt = conn.prepareStatement(sql)
+        val resultSet = stmt.executeQuery()
 
-        try {
+        resultSet.use { rs ->
             val minMaxValues = mutableSetOf<String>()
             while (rs.next()) {
                 minMaxValues.add(rs.getString(1))
                 minMaxValues.add(rs.getString(2))
             }
             return minMaxValues
-        } finally {
-            rs.close()
         }
     }
 
-    private fun getMinMaxValuesFromStore(store: ShapefileDataStore, attribute: String, tableName: String): MutableSet<String> {
-        LOGGER.info("Get min/max values from ShapefileDataStore");
-        var values = getDistinctValuesFromStore(store, attribute, tableName)
+    private fun getMinMaxValuesFromStore(store: ShapefileDataStore, attribute: String): MutableSet<String> {
+        LOGGER.info("Get min/max values from ShapefileDataStore")
+        val values = getDistinctValuesFromStore(store, attribute)
         return mutableSetOf(values.min(), values.max())
-    }
-
-    @Throws(IllegalArgumentException::class)
-    private fun unwrapDataStore(dataStore: ReadOnlyDataStore): ContentDataStore {
-        val dataStoreTypes = listOf(
-            JDBCDataStore::class.java,
-            ShapefileDataStore::class.java,
-        )
-
-        for (type in dataStoreTypes) {
-            try {
-                val unwrappedDataStore = dataStore.unwrap(type)
-                if (unwrappedDataStore != null) {
-                    return unwrappedDataStore
-                }
-            } catch (e: Exception) {
-                LOGGER.log(Level.FINE, "Could not unwrap data store of type ${type.simpleName}", e)
-            }
-        }
-        throw IllegalArgumentException("Could not unwrap data store")
     }
 
     @Throws(JsonProcessingException::class)
